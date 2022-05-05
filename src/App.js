cf1849e2091e81a8422664576d87d1b32960c904
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2022-05-05 19:04:01 +0300

    commit to master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import Application from './ApplicationForm';
 import Asiakkaat from './Asiakkaat';
 import Uniform from './Uniform';
+import ApplicationForm from './ApplicationForm';
+
+
 
 function App() {
   return (
     <div className="App">
       <Uniform/>
+      {/* <ApplicationForm/> */}
       {/* <Asiakkaat/> */}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+/* import './App.scss'; */
 
 //import Asiakkaat from './Asiakkaat';
 import Welcome_Hogwarts from './Welcome_Hogwarts';
 import Yhteystiedot from './Yhteystiedot';
+import ApplicationForm from './ApplicationForm';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Application from './ApplicationForm';
 
 //En saanut sitä open esimerkkiä toimimaan, mutta alla olevalla (Verrattain samalla) räpellyksellä homma pyörii
 ReactDOM.render(
@@ -18,12 +21,14 @@ ReactDOM.render(
         <nav id="navi">
           <Link to="/">Welcome to Hogwarts</Link>
           <Link to="/asiakkaat">Books and equipments</Link>
+          <Link to="/ApplicationForm">Application</Link>
           {/* <Link to="/yhteystiedot" id="asiakas"></Link> */}
         </nav>
 
         <Routes>
           <Route path="/" element={<Welcome_Hogwarts />}/>
           <Route exact path="/asiakkaat" element={<App />}/>
+          <Route exact path= "/applicationform" element={<ApplicationForm />}/>
           <Route path="/asiakas/:id" element={<Yhteystiedot />}/>
         </Routes>
 
