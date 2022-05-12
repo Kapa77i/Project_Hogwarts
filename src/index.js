6569876747dded2a6d4ef72d8dece9d6f9aa5ff7
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2022-05-12 16:59:38 +0300

    Pientä fiksailua

--- src/index.js
+++ src/index.js
@@ -7,16 +7,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 /* import './App.scss'; */
 
-//import Asiakkaat from './Asiakkaat';
 import Welcome_Hogwarts from './Welcome_Hogwarts';
-import Yhteystiedot from './Yhteystiedot';
 import ApplicationForm from './ApplicationForm';
 import Books_Equipment from './Books_Equipment';
-
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Application from './ApplicationForm';
 
-//En saanut sitä open esimerkkiä toimimaan, mutta alla olevalla (Verrattain samalla) räpellyksellä homma pyörii
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
@@ -25,15 +21,12 @@ createRoot(document.getElementById('root')).render(
           <Link to="/">Welcome to Hogwarts</Link>
           <Link to="/ApplicationForm">Application</Link>
           <Link to="/Books_Equipment">Books and equipments</Link>
-          
-          {/* <Link to="/yhteystiedot" id="asiakas"></Link> */}
         </nav>
 
         <Routes>
           <Route path="/" element={<Welcome_Hogwarts />}/>
           <Route exact path= "/applicationform" element={<ApplicationForm />}/>
           <Route exact path="/books_equipment" element={<App />}/>
-          <Route path="/wizards/:id" element={<Yhteystiedot />}/>
         </Routes>
 
       </Router>
